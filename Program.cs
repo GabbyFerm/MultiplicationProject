3045de849e07bdbd6aa60d8bc3bb7f985fbf0fbb
Gabby <gabbzf@gmail.com>
2024-10-01 16:22:19 +0200

    Add files via upload
    Cleaned up the code

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+namespace MultiplicationProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to generate it's multiplication table: ");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Multiplication table for {userNumber}");
+
+            Console.WriteLine();
+
+            for (int interation = 1; interation <=10; interation++) 
+            {
+
+                int multiplicationResult = userNumber * interation;
+                Console.WriteLine($"{userNumber} x {interation} = {multiplicationResult}");
+            }
+        }
+    }
+}
